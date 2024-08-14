8b137a0a7188b4a2a40c8362b8af850dbf2ff51e
Kiran Khayamali <107056117+KiranKhayamali@users.noreply.github.com>
2024-08-14 20:27:02 +0545

    Update assignment2.py
    adding exception handling

--- assignment(Kiran Khayamali)/assignment2.py
+++ assignment(Kiran Khayamali)/assignment2.py
@@ -69,4 +69,7 @@ def register(flag):
         print("Choose 1 or 2 to run the program:")
 
 user_input = int(input("Enter 1 to sign up and 2 to sign in :"))
-register(user_input)
+try:
+    register(user_input)
+except Exception as e:
+    print(e)
